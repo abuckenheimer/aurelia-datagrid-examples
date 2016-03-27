a85548c34b0e1a7038703ecf3d3475c0443f08f7
Donnell Jenkins <djenkins@donthedev.com>
2016-03-27 10:51:08 -0500

    feat(main): Added development logging

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 export function configure(aurelia) {
   aurelia.use
     .standardConfiguration()
+    .developmentLogging()
     .plugin('donnelljenkins/aurelia-pagination')
     .plugin('donnelljenkins/aurelia-datagrid');
 
